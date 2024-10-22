1e61a0431bf554eb9b44510f86092aab4ff584a4
kizuyoko <kizuyoko@gmail.com>
2024-10-22 18:14:58 +0200

    vercel deploy issue

--- src/util/dummy_data.tsx
+++ src/util/dummy_data.tsx
@@ -5,16 +5,19 @@ export const DUMMYDATA: Music[]  = [
     name: 'Music 1, Music 1, Music 1, Music 1!',
     artist: 'Artist 1, Artist 1, Artist 1, Artist 1, Artist 1, Artist 1, Artist 1, Artist 1, Artist 1, Artist 1',
     album: 'Album 1',
-    id: 'm1'
+    id: 'm1',
+    uri: 'sss'
   }, {
     name: 'Music 2',
     artist: 'Artist 2',
     album: 'Album 2',
-    id: 'm2'
+    id: 'm2',
+    uri: 'sss'
   }, {
     name: 'Music 3',
     artist: 'Artist 3',
     album: 'Album 3',
-    id: 'm3'
+    id: 'm3',
+    uri: 'sss'
   },
 ];
